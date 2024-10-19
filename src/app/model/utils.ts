d781f33acdafed3ad98a788ee972b9a4bdd5bb92
ZakyVreel <zakariya.saoula@gmail.com>
2024-10-19 21:25:29 +0200

    :art: add ms to global leaderboards

--- src/app/model/utils.ts
+++ src/app/model/utils.ts
@@ -32,6 +32,45 @@ export function formatTime(ms: number): string {
     return result;
   }
 
+  export function formatTimeWithMilliseconds(ms: number): string {
+    // Convertir en secondes entières
+    const totalSeconds = Math.floor(ms / 1000);
+    
+    // Extraire les heures, minutes, secondes et millisecondes
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    const milliseconds = ms % 1000;
+  
+    // Construire le format conditionnel
+    let result = '';
+  
+    // Afficher les heures seulement si elles sont supérieures à 0
+    if (hours > 0) {
+      result += `${hours}:`;
+    }
+  
+    // Ajouter les minutes avec un zéro devant si elles sont inférieures à 10, sauf s'il y a des heures
+    if (minutes < 10 && hours > 0) {
+      result += `0${minutes}:`;
+    } else {
+      result += `${minutes}:`;
+    }
+  
+    // Ajouter les secondes avec un zéro devant si elles sont inférieures à 10
+    if (seconds < 10) {
+      result += `0${seconds}`;
+    } else {
+      result += `${seconds}`;
+    }
+
+    // Ajouter les millisecondes avec trois chiffres
+    result += `.${milliseconds.toString().padStart(3, '0')}`;
+  
+    return result;
+}
+
+
   export function formatUUID(uuid: string): string {
     // Assurez-vous que l'UUID fait bien 32 caractères
     if (uuid.length !== 32) {
